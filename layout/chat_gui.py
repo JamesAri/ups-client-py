aebf595a7a28060568e62975377e23cdc9b27c16
Jakub Slechta <jakub.slechta@emplifi.io>
2022-09-28 15:26:54 +0200

    GUI DONE

--- layout/chat_gui.py
+++ layout/chat_gui.py
@@ -11,13 +11,12 @@ def draw_input_bg(win, active):
                      INPUT_REC.w + 2 * input_border_padding,
                      INPUT_REC.h + 2 * input_border_padding,
                  ),
-                 width=2, border_radius=2)
+                 width=input_border_padding, border_radius=2)
 
 
 def make_input(win, font, chat, make_input_tick):
     text_surface = font.render(chat.current_text, True, BLACK)
-    win.blit(text_surface, (CHAT_RECT.x + TEXT_PADDING,
-                            CHAT_RECT.y + CHAT_RECT.height - text_surface.get_height() - TEXT_PADDING))
+    win.blit(text_surface, (INPUT_REC.x + CHAT_INPUT_TEXT_PADDING, INPUT_REC.y))
     if make_input_tick:
         do_input_tick(win, text_surface)
 
@@ -37,6 +36,7 @@ def draw_chat_layout(win, active, font, chat):
 
     # BORDER
     pg.draw.rect(win, BLACK, CHAT_RECT, width=1, border_radius=5)
+
     # INPUT
     draw_input_bg(win, active)
 
@@ -44,12 +44,12 @@ def draw_chat_layout(win, active, font, chat):
 def do_input_tick(win, text_surface):
     pg.draw.line(win, BLACK,
                  (
-                     CHAT_RECT.x + TEXT_PADDING + text_surface.get_width() + 1,
-                     CHAT_RECT.y + CHAT_RECT.height - text_surface.get_height() - TEXT_PADDING + 3,
+                     INPUT_REC.x + text_surface.get_width() + CHAT_INPUT_TEXT_PADDING + 1,
+                     INPUT_REC.y + 3,
                  ),
                  (
-                     CHAT_RECT.x + TEXT_PADDING + text_surface.get_width() + 1,
-                     CHAT_RECT.y + CHAT_RECT.height - text_surface.get_height() - TEXT_PADDING + text_surface.get_height() - 3
+                     INPUT_REC.x + text_surface.get_width() + CHAT_INPUT_TEXT_PADDING + 1,
+                     INPUT_REC.y + INPUT_REC.h - 3,
                  ))
 
 
