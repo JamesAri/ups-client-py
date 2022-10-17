6cf718682d08ea19c394b1b2b68f9f4e405813bc
Jakub Slechta <jakub.slechta@emplifi.io>
2022-10-17 16:05:51 +0200

    hehe

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ def get_client() -> Client:
 
 if __name__ == "__main__":
     client: Client = get_client()
-
+    print(1)
     client.login()
-
+    print(2)
     receive_thread = threading.Thread(target=client.receive)
+    print(3)
     receive_thread.start()
-
+    print(4)
     start_game(client)
-
+    print(5)
     receive_thread.join()
+    print(6)
 
 # TODO: server - buffer - 1 per instance
 # TODO: client - send deltas, not whole canvas
